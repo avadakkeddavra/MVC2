b0c719a2e95580bac1d2bc8d00f75f8b860c2f02
avaddakkedavra <avaddakkedavra@gmail.com>
2017-04-20 16:57:44 +0300

    parser

--- controllers/WeatherController.php
+++ controllers/WeatherController.php
@@ -1,33 +1,37 @@
 <?php 
- use GuzzleHttp\Client;
- use GuzzleHttp\EntityBody;
- use GuzzleHttp\Psr7\Request;
- use GuzzleHttp\Message\Response;
- use GuzzleHttp\Subscriber\Retry\RetrySubscriber;
 
-	/**
-	* 
-	*/
+
 	class WeatherController extends Controller
 	{
 		public function indexAction()
 		{
-			$client = new Client([
-			    // Base URI is used with relative requests
-			    'base_uri' => 'https://yandex.ru',
-			    // You can set any number of default request options.
-			    'timeout'  => 10.0,
-			]);
+			$html = file_get_html('https://www.gismeteo.ua/weather-zaporizhia-5093/');
 			
+			$table = $html->find("#tbwdaily1",0);
 			
+			$night = $html->find("#tbwdaily1", 0)->children(0)->children(3);
+			$morning = $html->find("#tbwdaily1", 0)->children(1)->children(3);
+			$day = $html->find("#tbwdaily1", 0)->children(2)->children(3);
+			$evening = $html->find("#tbwdaily1", 0)->children(3)->children(3);
+			echo $night;
+			echo $morning;
+			echo $day;
+			echo $evening;
+			
+			$data = array(
+				'temperature' => array(
+					'night'   => $night,
+					'morning' => $morning,
+					'day'     => $day,
+					'evening' => $evening
+					),
 
+				);
 			
-			$response = $client->request('GET');
-
-			echo '<pre>';
-		    echo $response->getBody();
-		    echo '</pre>';
-	
+			foreach($data['temperature'] as $item)
+			{
+				echo $item;
+			}
 		}
 	}
 
--- load.php
+++ load.php
@@ -4,7 +4,7 @@ require_once 'core/Routing.php';
 require_once 'core/Model.php';
 require_once 'core/View.php';
 require_once 'core/Controller.php';
-require_once 'vendor/autoload.php';
+require_once 'vendor2/simple_html_dom.php';
 //Запуск роутинга
 Routing::execute();
 
